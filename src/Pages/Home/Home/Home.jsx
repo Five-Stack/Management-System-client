0f2422ffc4564ada7fb54deb2e23cc4a556e82e1
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2023-10-16 16:58:18 +0600

    set title on labsection

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Department from '../DepartmentSection/Department'
+import LabSection from '../LabSection/LabSection'
 
 const Home = () => {
   return (
     <>
       <div className='text-center font-bold text-red-400 text-2xl mt-5'>This Is Our Home Page:)</div>
       <Department></Department>
+      <LabSection/>
     </>
   )
 }
---
+++ src/Pages/Home/LabSection/LabSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../../../Components/SharedComponents/Title/Title'
+
+function LabSection() {
+  return (
+    <div>
+        <Title text="আমাদের শ্রেণীকক্ষ  ও ল্যাব সমূহ "/>
+      
+    </div>
+  )
+}
+
+export default LabSection
