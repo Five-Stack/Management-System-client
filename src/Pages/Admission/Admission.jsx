577c9d0f19d94702233db1003df22cdf1a889b68
Shihab066 <md.shihab066@gmail.com>
2023-10-16 21:58:08 +0600

    admission page added

---
+++ src/Pages/Admission/Admission.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Admission = () => {
+    return (
+        <div className='lg-container'>
+
+            <div className='max-w-6xl mx-auto bg-white my-16 px-28 py-20'>
+                <h3 className='font-medium text-3xl text-center pb-10'>এডমিশন</h3>
+                <form>
+                    <div className='grid grid-cols-2 gap-x-3 gap-y-10'>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                        </div>
+                    </div>
+                    <div className='text-right mt-8 pr-1'>
+                        <button type='submit' className='text-sm bg-primary txt-white px-6 py-2 rounded-md'>জমা দিন</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    );
+};
+
+export default Admission;
--- src/Routes/Route/Route.jsx
+++ src/Routes/Route/Route.jsx
@@ -4,6 +4,7 @@ import DashboardLayout from "../../Layout/DashboardLayout";
 import ErrorPage from "../../Components/ErrorPage/ErrorPage";
 import About from "../../Pages/About/About";
 import Home from "../../Pages/Home/Home/Home";
+import Admission from "../../Pages/Admission/Admission";
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
       {
         path: '/about',
         element: <About />
+      },
+      {
+        path: 'admission',
+        element: <Admission />
       }
     ]
   },
