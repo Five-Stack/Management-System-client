05ddbf61938979377b4971a50fedf5bdd0fbb72b
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2023-10-17 13:28:58 +0600

    banner card is complete

---
+++ src/Pages/Home/BannerSection/BannerCard.jsx
@@ -0,0 +1,39 @@
+
+
+function BannerCard() {
+    // 
+    const cards = [
+        {
+            image: "https://i.ibb.co/nLb2Cb0/teacher.png",
+            name: "শিক্ষক",
+            number: '৪০'
+        },
+        {
+            image: "https://i.ibb.co/ZM03fL4/graduation-3902189.png",
+            name: "A+ শিক্ষার্থী",
+            number: '২০০'
+        },
+        {
+            image: "https://i.ibb.co/pn2H4CZ/cheer-up-5435714.png",
+            name: "শিক্ষার্থী",
+            number: '৪০০'
+        }
+    ]
+    return (
+        <div className="w-[60%] bg-[#115ED0] p-8 relative left-1/2 -translate-x-1/2 bottom-12 rounded-lg shadow-md grid md:grid-cols-2 lg:grid-cols-3 items-center gap-6">
+            {
+                cards.map((item, index) =>
+                    <div key={index} className="p-2 bg-[rgba(217,217,217,0.27)] text-white flex items-center gap-2 rounded-md">
+                        <img src={item?.image} className="h-20" alt="" />
+                        <div>
+                            <h1 className="text-3xl mb-1">{item?.number}+</h1>
+                            <h1>{item?.name}+</h1>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default BannerCard
--- src/Pages/Home/BannerSection/BannerSection.jsx
+++ src/Pages/Home/BannerSection/BannerSection.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { Button } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { FaGraduationCap } from 'react-icons/fa6';
+import BannerCard from './BannerCard';
 
 function BannerSection() {
     let prevIndex = 0;
@@ -18,7 +19,7 @@ function BannerSection() {
         return () => {
             clearInterval(interval);
         };
-    }, []);
+    }, [imageUrls]);
 
     useEffect(() => {
         // Load the current image when the index changes
@@ -36,15 +37,16 @@ function BannerSection() {
 
 
     return (
-        <div className='w-full my-16 relative'>
+        <div className='w-full relative'>
             <img className='h-[543px]' src={imageUrls[currentImageIndex]?.bannerImg} alt="" />
             <div className="h-full w-full bg-gradient-to-r from-[rgba(255,255,255,1)] to-transparent absolute top-0 left-0">
-                <div className="lg-container h-full w-full flex flex-col justify-center items-start">
+                <div className="lg-container h-full w-full flex flex-col justify-center items-start -translate-y-20">
                     <h1 className='text-[#1B2637] text-[42px] w-[60%] font-bold'><span className='text-[#2A76E7]'>সাইক</span> ইনস্টিটিউট  এন্ড মেনেজমেন্ট টেকনোলজি </h1>
                     <p className='text-xl mt-2'>Best Polytechnic Institute In Dhaka</p>
-                    <Button className='text-white bg-[#2A76E7] mt-10 font-semibold'><FaGraduationCap className='text-2xl mr-1' /><span>এডমিশন</span></Button>
+                    <Button className='text-white bg-[#2A76E7] mt-10 font-semibold enabled:hover:bg-[#2A76E7]'><FaGraduationCap className='text-2xl mr-1' /><span>এডমিশন</span></Button>
                 </div>
             </div>
+            <BannerCard/>
         </div>
     )
 }
