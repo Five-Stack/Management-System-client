0258f9a84b17fe519deab1070c9d6189c0a8f589
Shihab066 <md.shihab066@gmail.com>
2023-10-17 18:30:03 +0600

    admission form design addec

--- src/Pages/Admission/Admission.jsx
+++ src/Pages/Admission/Admission.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Admission = () => {
+   
     return (
         <div className='lg-container'>
 
@@ -9,52 +11,77 @@ const Admission = () => {
                 <form>
                     <div className='grid grid-cols-2 gap-x-3 gap-y-10'>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">টেকনোলজির নাম *</label>
+                            <select id="countries" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+                                <option selected disabled>Choose a country</option>
+                                <option value="US">United States</option>
+                                <option value="CA">Canada</option>
+                                <option value="FR">France</option>
+                                <option value="DE">Germany</option>
+                            </select>
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">আবেদনকারীর নাম *</label>
+                            <input type="text" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="আবেদনকারীর নাম লিখুন" required />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">পিতার নাম *</label>
+                            <input type="text" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="পিতার নাম লিখুন" required />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">মাতার নাম *</label>
+                            <input type="text" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="মাতার নাম লিখুন " required />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">জন্মতারিখ *</label>
+                            <input type="date" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="" required />
+                            
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">ইমেইল</label>
+                            <input type="text" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="ইমেইল লিখুন" required />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">ফোন নাম্বার *</label>
+                            <input type="text" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="ফোন নাম্বার  লিখুন" required />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">ঠিকানা *</label>
+                            <input type="text" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="ঠিকানা লিখুন" required />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">পরীক্ষার নাম *</label>
+                            <select id="countries" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+                                <option defaultValue>Choose a country</option>
+                                <option value="US">United States</option>
+                                <option value="CA">Canada</option>
+                                <option value="FR">France</option>
+                                <option value="DE">Germany</option>
+                            </select>
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">গ্রুপ / সাবজেক্ট  *</label>
+                            <select id="countries" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+                                <option defaultValue>Choose a country</option>
+                                <option value="US">United States</option>
+                                <option value="CA">Canada</option>
+                                <option value="FR">France</option>
+                                <option value="DE">Germany</option>
+                            </select>
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">পাসের সন *</label>
+                            <select id="countries" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+                                <option defaultValue>Choose a country</option>
+                                <option value="US">United States</option>
+                                <option value="CA">Canada</option>
+                                <option value="FR">France</option>
+                                <option value="DE">Germany</option>
+                            </select>
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">First name</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+                            <label className="block mb-2 text-sm txt-blue font-medium dark:text-white">শ্রেণী/বিভাগ/গ্রেড *</label>
+                            <input type="text" id="first_name" className="bg-[#D9D9D952] border-2 focus:border border-opacity-60 border-blue-500 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
                         </div>
                     </div>
                     <div className='text-right mt-8 pr-1'>
@@ -66,5 +93,4 @@ const Admission = () => {
         </div>
     );
 };
-
 export default Admission;
