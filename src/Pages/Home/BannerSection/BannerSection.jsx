c806a16deb51def13656b2668c5415cc8213be7d
Shihab066 <md.shihab066@gmail.com>
2023-10-18 12:19:41 +0600

    set banner section admission button path

--- src/Pages/Home/BannerSection/BannerSection.jsx
+++ src/Pages/Home/BannerSection/BannerSection.jsx
@@ -3,6 +3,7 @@ import { Button } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { FaGraduationCap } from 'react-icons/fa6';
 import BannerCard from './BannerCard';
+import { Link } from 'react-router-dom';
 
 function BannerSection() {
     let prevIndex = 0;
@@ -26,7 +27,7 @@ function BannerSection() {
         const fetchImage = () => {
             try {
                 axios.get('https://management-system-server.vercel.app/api/sliderbgs')
-                .then(data => setImageUrls(data.data))
+                    .then(data => setImageUrls(data.data))
             } catch (error) {
                 console.error('Error fetching image:', error);
             }
@@ -42,11 +43,15 @@ function BannerSection() {
             <div className="h-full w-full bg-gradient-to-r from-[rgba(255,255,255,1)] to-transparent absolute top-0 left-0">
                 <div className="lg-container h-full w-full flex flex-col justify-center items-start -translate-y-20">
                     <h1 className='text-[#1B2637] text-[42px] w-[60%] font-bold'><span className='text-[#2A76E7]'>সাইক</span> ইনস্টিটিউট  এন্ড মেনেজমেন্ট টেকনোলজি </h1>
-                    <p className='text-xl mt-2'>Best Polytechnic Institute In Dhaka</p>
-                    <Button className='text-white bg-[#2A76E7] mt-10 font-semibold enabled:hover:bg-[#2A76E7]'><FaGraduationCap className='text-2xl mr-1' /><span>এডমিশন</span></Button>
+                    <p className='text-xl mt-2'>Best Polytechnic Institute In Dhaka</p>                    
+                    <Link to={'/admission'}>
+                        <button className='bg-primary hover:bg-blue-700 txt-white mt-10 px-5 py-3 rounded-md flex items-center transition-colors duration-100'>
+                            <FaGraduationCap className='text-2xl mr-1' />এডমিশন
+                        </button>
+                    </Link>
                 </div>
             </div>
-            <BannerCard/>
+            <BannerCard />
         </div>
     )
 }
